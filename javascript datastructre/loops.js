c9054edd9f3e31676e2b3a25488f042ef6ee6dcf
Mayank4543 <134360258+Mayank4543@users.noreply.github.com>
2023-11-11 23:37:08 +0530

    intial commit

---
+++ javascript datastructre/loops.js
@@ -0,0 +1,22 @@
+// q1 : sum of all nuatural number 1 to n
+// function sumofdigit(num) {
+//   //   sum = 0;
+//   //   for (var i = 1; i <= num; i++) {
+//   //     sum = sum + i;
+//   //   }
+//   //   return sum;
+//   return (num * (num + 1)) / 2;
+// }
+
+// console.log(sumofdigit(5));
+// console.log(sumofdigit(10));
+// Q2 sum of digit of a numberscd;
+// function sumofdigis(num) {
+//   let sum = 0;
+//   while (num > 0) {
+//     sum += num % 10;
+//     num = Math.floor(num / 10);
+//   }
+//   return sum;
+// }
+// console.log(sumofdigis(123456));
---
+++ practice/2.c++
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout << "enter number a:";
+    cin >> a;
+    cout << "enter number b:";
+    cin >> b;
+    cout << "enter number c:";
+    cin >> c;
+    float avg;
+    avg = (a + b + c) / 3;
+    cout << " the average of three number is :" << avg;
+    return 0;
+}
